0538054b49009ccd37d0c25b66425b38a25ff941
jhon10101 <jhonzambrano21@gmail.com>
2023-08-01 09:25:59 -0400

    update

--- index.php
+++ index.php
@@ -120,18 +120,18 @@
 
             <!-- Modal -->
             <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
 
-                    <div class="modal-body">
-                        <div id="datostv"></div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    
+                        <div class="modal-body">
+                            <div id="datostv"></div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        
+                        </div>
                     </div>
                 </div>
-            </div>
             </div>
     
 </body>
